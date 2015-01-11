e9bd3ed00df1ba9e64b1eaa7a2d03015c8968f25
PBRT <pierre.beard@workangel.com>
2015-01-11 23:51:02 +0000

    Slide menu v2

--- assets/general.js
+++ assets/general.js
@@ -4,8 +4,10 @@ function toggleMenu(){
 
 	if($('#container').hasClass('container--show')) {
 		$('#container').removeClass('container--show');
+		$('#brand').removeClass('navbar__brand--active')
 	} else {
 		$('#container').addClass('container--show');	
+		$('#brand').addClass('navbar__brand--active')
 	}
 
 ;}
